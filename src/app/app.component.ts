6252533941dc9672b3f5bc8633c16f49262f0f05
Manon Heine <manon.heine@incentro.com>
2024-06-11 19:36:32 +0200

    first tries, setup decent buttons, cards, bars. wip for steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem, PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { KnobModule } from 'primeng/knob';
+import { StepperModule } from 'primeng/stepper';
+import { StepsModule } from 'primeng/steps';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    DividerModule,
+    KnobModule,
+    FormsModule,
+    CardModule,
+    StepsModule,
+    StepperModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'prime-test';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  value: number = 31;
+
+  stepsItems: MenuItem[] | undefined;
+  stepActive: number = 2;
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+    this.stepsItems = [
+      {
+        label: 'Status 40',
+      },
+      {
+        label: 'Status 50',
+      },
+      {
+        label: 'Status 60',
+      },
+      {
+        label: 'Status 70',
+      },
+    ];
+  }
 }
